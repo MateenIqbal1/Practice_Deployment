609e3ead2a1abb25150111691f57e44de9b71232
MateenIqbal1 <mateeniqbal840@gmail.com>
2026-09-02 17:17:44 +0500

    testing nignix and deployment

--- app.js
+++ app.js
@@ -7,5 +7,6 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
+app.get("/", (req, res) => { res.send("Hello from EC2 + Nginx + Docker!"); });
 
 module.exports = app;
